ca43fdce1d14a97bdd8a3f14840b024476304177
James Stephens <j.l.stephens91@gmail.com>
2021-01-23 21:23:45 +0000

    Create flask app

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def helloWorld():
+    return "Hello world"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=os.environ.get("PORT"),
+            debug=True)
