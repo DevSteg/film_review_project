cdc55039683aaf40fc66264f3664a4aa5ec796ba
James Stephens <j.l.stephens91@gmail.com>
2021-02-14 16:17:42 +0000

    Add datepicker functionality

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.datepicker').datepicker({
+        format: "dd/mm/yyyy", 
+        showCleardBtn: true,
+        min: new Date(01,01,1920),
+        yearRange: [1920,2030]
+    });
+});
